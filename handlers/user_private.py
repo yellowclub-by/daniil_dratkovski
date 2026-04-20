cfd07ef48b76f9b7ac737753edcfec3a0ae17ed0
fizikus13836 <hello@yellowclub.by>
2026-04-20 18:03:24 +0300

    add filters

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,5 +1,5 @@
 from aiogram.filters import CommandStart, Command
-from aiogram import types, Router
+from aiogram import types, Router, F
 
 user_router = Router()
 
@@ -11,13 +11,9 @@ async def start_cmd(message: types.Message):
     /search - поиск музыки
     /pudge - СЕКРЕТ""")
 
-
-# @user_router.message()
-# async def echo(message: types.Message):
-#     answer = message.text
-#     await message.answer(answer)
-
-
+ # @user_router.message(F.text)
+ # async def image(message: types.message):
+ #     await message.answer('omagad')
 @user_router.message(Command('info'))
 async def info(message: types.message):
     await message.answer('тут была-бы ваша реклама')
@@ -25,9 +21,19 @@ async def info(message: types.message):
 
 @user_router.message(Command('search'))
 async def search(message: types.message):
-   await message.answer('рамштайн нэвэр дай')
+    await message.answer('рамштайн нэвэр дай')
 
 
 @user_router.message(Command('pudge'))
 async def pudge(message: types.message):
     await message.answer('PUDGE PUDGE PUDGE PUDGE!!!')
+
+# @user_router.message(F.text) # фильтр текста
+#@user_router.message(F.image) # фильтр kamtinki
+#@user_router.message(F.text.lower() == 'дистивка') # фильтр konkritnigo texsty
+#@user_router.message(F.text.lower().contains('Lamosa')) # фильтр po zovderjaniy
+@user_router.message(F.text.lower().endswith("?"))
+# @user_router.message(F.text.lower().startswith("как"), F.text.lower().endwith("?"))
+# @user_router.message(F.text.lower().startswith("как") | F.text.lower().endwith("?"))
+async def echo(message: types.Message):
+    await message.answer('Limo')
