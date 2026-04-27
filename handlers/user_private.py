ba6f33529b39efdd3dfc157e7d28d1ebc346b9b2
SeraphimTech <serafimmasiucovich@gmail.com>
2026-04-27 18:02:29 +0300

    feat:add reply keyboard

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,5 +1,8 @@
 from aiogram.filters import CommandStart, Command
 from aiogram import types, Router, F
+from aiogram.types import reply_markup_union
+
+from daniil_dratkovski.keyboards import reply
 
 user_router = Router()
 
@@ -9,31 +12,32 @@ async def start_cmd(message: types.Message):
     await message.answer("""ПРИВИТ МОЙ БОТ МОЖЕТ СЬЕСТЬ ТВОЕГО ХОМЯЧКА
     /info - информация о боте
     /search - поиск музыки
-    /pudge - СЕКРЕТ""")
+                         """,reply_markup=reply.start_kb)
 
- # @user_router.message(F.text)
- # async def image(message: types.message):
- #     await message.answer('omagad')
+@user_router.message(Command('start'))
+async def second_start(message: types.message):
+    await message.answer
+@user_router.message(F.text.lower().endswith('?'))
+@user_router.message(F.text.lower().contains('что делает'))
+@user_router.message(F.text.lower().contains('инфо'))
+@user_router.message(F.text.lower().contains('старт'))
 @user_router.message(Command('info'))
 async def info(message: types.message):
-    await message.answer('тут была-бы ваша реклама')
+    await message.answer('тут была-бы ваша реклама', reply_markup=reply.main_kb)
 
 
+@user_router.message(F.text.lower().contains("иск"))
+@user_router.message(F.text.lower().contains('поиск'))
 @user_router.message(Command('search'))
 async def search(message: types.message):
     await message.answer('рамштайн нэвэр дай')
 
-
-@user_router.message(Command('pudge'))
-async def pudge(message: types.message):
-    await message.answer('PUDGE PUDGE PUDGE PUDGE!!!')
-
 # @user_router.message(F.text) # фильтр текста
-#@user_router.message(F.image) # фильтр kamtinki
-#@user_router.message(F.text.lower() == 'дистивка') # фильтр konkritnigo texsty
-#@user_router.message(F.text.lower().contains('Lamosa')) # фильтр po zovderjaniy
-@user_router.message(F.text.lower().endswith("?"))
+# @user_router.message(F.image) # фильтр kamtinki
+# @user_router.message(F.text.lower() == 'дистивка') # фильтр konkritnigo texsty
+# @user_router.message(F.text.lower().contains('Lamosa')) # фильтр po zovderjaniy
+# @user_router.message(F.text.lower().endswith("?"))
 # @user_router.message(F.text.lower().startswith("как"), F.text.lower().endwith("?"))
 # @user_router.message(F.text.lower().startswith("как") | F.text.lower().endwith("?"))
 async def echo(message: types.Message):
-    await message.answer('Limo')
+    await message.answer('Limosa')
---
+++ keyboards/reply.py
@@ -0,0 +1,15 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main_kb = ReplyKeyboardMarkup(
+    keyboard=
+    [
+        [KeyboardButton(text="оалвдаов"), KeyboardButton(text="jkljsd")],
+        [KeyboardButton(text="bubububub")]
+    ]
+)
+
+start_kb = ReplyKeyboardMarkup(
+    keyboard=   [
+        [KeyboardButton(text='Старт')]
+    ]
+)
